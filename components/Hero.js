36684a66879b823a3764ad4c8a8d583da72f716d
Morad17 <morad123@hotmail.co.uk>
2022-04-29 18:32:56 +0100

    updates

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero-banner-container">
+      <div className="section-1">
+        <p className="product-text">
+          Small Text
+        </p>
+        <h3>Mid Text</h3>
+        <img src="" alt="product" className="hero-image"/>
+        <Link href="/product/ID" >
+          <button type="button">Button</button>
+        </Link>
+        </div>
+        <div className="section-2">
+          
+          <div className="desc">
+            <h5>Description</h5>
+            <p>Description</p>
+          </div>
+        </div>
+      
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import { client } from '../lib/client';
+
 import Hero from '../components/Hero'
 import FooterHero from '../components/FooterHero'
 import Product from '../components/Product'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       <Hero />
@@ -14,15 +16,28 @@ const Home = () => {
       </div>
 
       <div className="">
-      {/* {['Product1'].map(
-        (product) => product
+      { products?.map(
+        (product) => product.name
       )
-      } */}
+      }
       </div>
 
       <FooterHero />
     </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
+}
+
 
 export default Home

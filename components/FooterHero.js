8d9585a808586aef49193b9b4337b8510586b587
Morad17 <morad123@hotmail.co.uk>
2022-04-29 03:04:30 +0100

    setting up home

---
+++ components/FooterHero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FooterHero = () => {
+  return (
+    <div>FooterHero</div>
+  )
+}
+
+export default FooterHero
---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as Footer } from './Footer';
+export { default as Basket } from './Basket';
+export { default as FooterHero } from './FooterHero';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as Product } from './Product';
